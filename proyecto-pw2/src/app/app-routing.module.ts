a86784a7117f2f7f60bac3b8d970787988a195b3
Gustv87 <tavomartinez87@gmail.com>
2020-03-27 17:53:06 -0600

    ...

--- proyecto-pw2/src/app/app-routing.module.ts
+++ proyecto-pw2/src/app/app-routing.module.ts
@@ -4,15 +4,16 @@ import { UsersComponent } from './users/users.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { TeamsComponent } from './teams/teams.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth/auth.guard';
 
 
 
 const routes: Routes = [
-  {path: 'users', component:UsersComponent},
-  {path: 'projects', component:ProjectsComponent },
-  {path: 'teams', component:TeamsComponent},
-  {path: 'login',component:LoginComponent},
- 
+  { path: 'users', component: UsersComponent, canActivate:  [AuthGuard] },
+  { path: 'projects', component: ProjectsComponent, canActivate:  [AuthGuard], data: {adminRequired : true} },
+  { path: 'teams', component: TeamsComponent ,canActivate:  [AuthGuard]},
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
